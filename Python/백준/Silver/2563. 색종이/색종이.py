35e1f79d5d68be14a89e7b61851986448102c0e4
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2025-11-20 04:12:41 +0900

    [Silver V] Title: 색종이, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/2563. 색종이/색종이.py
@@ -0,0 +1,16 @@
+board = [[0] * 100 for _ in range(100)]
+
+N = int(input())
+
+for _ in range(N) :
+    x, y = map(int,input().split())
+    for i in range(x,x+10):
+        for j in range(y,y+10) :
+            board[i][j] = 1
+            
+area = 0
+for i in range(100) :
+    for j in range(100) :
+        area += board[i][j]
+print(area)
+            
