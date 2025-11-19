70b51b3918054eaae9e1bfd20123964896c80835
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2025-11-19 23:01:52 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,26 @@
+grade_map = {
+    "A+": 4.5,
+    "A0": 4.0,
+    "B+": 3.5,
+    "B0": 3.0,
+    "C+": 2.5,
+    "C0": 2.0,
+    "D+": 1.5,
+    "D0": 1.0,
+    "F": 0.0
+}
+
+total_score = 0.0  # 분자: 학점 * 점수 합
+total_credit = 0.0 # 분모: 학점 합 (P 제외)
+
+for _ in range(20):
+    name, credit, grade = input().split()
+    credit = float(credit)
+
+    if grade == "P":        # P 과목은 계산에서 제외
+        continue
+
+    total_score += credit * grade_map[grade]
+    total_credit += credit
+
+print(total_score / total_credit)
