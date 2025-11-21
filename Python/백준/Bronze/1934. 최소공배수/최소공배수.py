5ce990e098b7db938e8059507ac9e59534d2f725
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2025-11-22 04:52:57 +0900

    [Bronze I] Title: 최소공배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+T = int(input().strip())
+
+for _ in range(T):
+    a, b = map(int, input().split())
+    g = gcd(a, b)
+    print(a * b // g)
