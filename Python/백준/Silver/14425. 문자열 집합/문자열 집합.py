c3cac8426ae7eb71e7d4885c9b4eb3e420025fd0
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2025-11-22 02:03:38 +0900

    [Silver IV] Title: 문자열 집합, Time: 88 ms, Memory: 37440 KB -BaekjoonHub

---
+++ Python/백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+S = {input().strip() for _ in range(N)}
+
+cnt = 0
+for _ in range(M):
+    word = input().strip()
+    if word in S:
+        cnt += 1
+
+print(cnt)
