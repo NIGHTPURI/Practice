0b3715f5c5b7364b4824a8ac4d9d6d923c8037b7
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2025-11-21 20:14:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ Python/백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,16 @@
+N = int(input().strip())
+
+count = 0
+
+while True:
+    if N % 5 == 0:
+        count += N // 5
+        print(count)
+        break
+    
+    N -= 3
+    count += 1
+
+    if N < 0:
+        print(-1)
+        break
