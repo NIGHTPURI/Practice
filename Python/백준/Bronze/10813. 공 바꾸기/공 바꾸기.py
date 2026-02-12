17a5bc39cb513a9a6f4865aedff1f3f32d708abf
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2026-02-12 20:39:31 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ Python/백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,13 +1,6 @@
-n, m = map(int,input().split())
-li = [i for i in range(1,n+1)]
-
-for _ in range(m) :
-    a, b = map(int,input().split())
-    a -= 1
-    b -= 1
-    
-    li[a], li[b] = li[b], li[a]
-    
-    
+N, M = map(int,input().split())
+li = [k for k in range(1, N+1)]
+for _ in range(M) :
+    i, j = map(int,input().split())
+    li[i-1] , li[j-1] = li[j-1] , li[i-1]
 print(*li)
-    
