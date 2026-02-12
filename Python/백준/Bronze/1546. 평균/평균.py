ea6cd2612e4b854c95b6955a03251d718a9ba4f4
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2026-02-13 02:15:42 +0900

    [Bronze I] Title: 평균, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/1546. 평균/평균.py
+++ Python/백준/Bronze/1546. 평균/평균.py
@@ -1,6 +1,5 @@
-N= int(input())
+N = int(input())
 li = list(map(int,input().split()))
 M = max(li)
-for i in range(len(li)) :
-    li[i] = li[i]/M*100
-print(sum(li)/N)    
+
+print(sum(li) /M*100 /N)
