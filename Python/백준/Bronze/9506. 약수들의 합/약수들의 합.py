e6568215c535d32477cf82be0c42e1b8452e1c23
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2026-02-26 18:24:09 +0900

    [Bronze I] Title: 약수들의 합, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ Python/백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,25 +1,24 @@
 while True:
     n = int(input())
-    if n == -1:      # 종료 조건
+    if n == -1:      
         break
 
-    if n == 1:       # 1은 완전수가 아님
+    if n == 1:       
         print("1 is NOT perfect.")
         continue
 
-    divisors = [1]   # 1은 항상 약수
+    divisors = [1]   
 
-    # 2부터 √n 까지만 검사해서 약수 찾기 (효율적인 방식)
     i = 2
     while i * i <= n:
         if n % i == 0:
             divisors.append(i)
-            if i != n // i:       # 서로 다른 약수 쌍이면 둘 다 추가
+            if i != n // i:       
                 divisors.append(n // i)
         i += 1
-
-    if sum(divisors) == n:
+    if sum(divisors) == n :
         divisors.sort()
-        print(f"{n} = " + " + ".join(map(str, divisors)))
-    else:
+        divisors_str = " + ".join(map(str,divisors))
+        print(f"{n} = {divisors_str}")
+    else :
         print(f"{n} is NOT perfect.")
