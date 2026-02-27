cade247fd9fa4e398ddb9e19e95e1330e5df14fd
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2026-02-27 17:46:01 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/2798. 블랙잭/블랙잭.py
+++ Python/백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -3,15 +3,16 @@ card = list(map(int,input().split()))
 
 best = 0
 
-for i in range(N):
-    for j in range(i+1,N):
-        for k in range(j+1,N):
-            s = card[i] + card[j] + card[k]
-            if s == M :
-                best = M
-                print(best)
+for i in range(N) :
+    for j in range(i+1,N) :
+        for k in range(j+1,N) :
+            t = card[i]+card[j]+card[k]
+            if t == M :
+                print(t)
                 exit()
-            if s < M and s > best :
-                best = s
+            if best < t < M :
+                best = t
+
 print(best)
+                
             
