3e296bfb9a8361407866b2c1ec9dc4931e268a87
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2026-03-09 17:16:04 +0900

    [Silver IV] Title: 스택 2, Time: 1060 ms, Memory: 72404 KB -BaekjoonHub

--- Python/백준/Silver/28278. 스택 2/스택 2.py
+++ Python/백준/Silver/28278. 스택 2/스택 2.py
@@ -1,31 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
+T = int(input().strip())
 stack = []
-out = []  
-
-for _ in range(N):
-    cmd = input().split()
-    
-    if cmd[0] == '1' :
-        stack.append(int(cmd[1]))
-    elif cmd[0] == '2' :
+out = []
+for t in range(1,T+1):
+    cmd = list(map(int, input().split()))
+    if cmd[0] == 1 :
+        stack.append(cmd[1])
+    elif cmd[0] == 2 :
         if stack :
-            out.append(str(stack.pop()))
+            out.append(stack.pop())
         else :
             out.append('-1')
-    elif cmd[0] == '3' :
-        out.append(str(len(stack)))
-    elif cmd[0] == '4' :
-        if not stack :
-            out.append('1')
-        else :
+    elif cmd[0] == 3 :
+        out.append(len(stack))
+    elif cmd[0] == 4 :
+        if stack :
             out.append('0')
-    elif cmd[0] == '5' :
+        else :
+            out.append('1')
+    elif cmd[0] == 5 :
         if stack :
-            out.append(str(stack[-1]))
+            out.append(stack[-1])
         else :
             out.append('-1')
-                       
-print('\n'.join(out))
+print(*out)
+    
