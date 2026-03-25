78dea48b939265ead213cd294a493d2deba8937b
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2026-03-25 18:19:24 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+def fibo(x) :
+    if x == 0 :
+        return 0
+    if x == 1 :
+        return 1
+    return fibo(x-1) + fibo(x-2)
+
+print(fibo(int(input())))
