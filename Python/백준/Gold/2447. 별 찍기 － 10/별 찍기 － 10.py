79209ab65565c9945a539851cb642709096392ae
Park Mireu <127381985+NIGHTPURI@users.noreply.github.com>
2026-03-27 16:06:24 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 45984 KB -BaekjoonHub

---
+++ Python/백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def draw_star(n) :
+    if n == 1 :
+        return ['*']
+    
+    stars = draw_star(n//3)
+    L = []
+    
+    for s in stars :
+        L.append(s * 3)
+    for s in stars :
+        L.append(s + ' '*(n//3) + s)
+    for s in stars :
+        L.append(s * 3)
+        
+    return L
+
+result = draw_star(int(input().strip()))
+print('\n'.join(result))
+    
